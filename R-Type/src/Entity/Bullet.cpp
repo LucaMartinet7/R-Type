ea1fc245b34f3cf56f488fb80d6ddb150a508fdc
Tounn10 <etienne.namur@epitech.eu>
2025-01-18 19:01:59 +0100

    fix: component added in the wrong registry

--- R-Type/src/Entity/Bullet.cpp
+++ R-Type/src/Entity/Bullet.cpp
@@ -12,10 +12,10 @@
 #include <iostream>
 
 Bullet::Bullet(Registry registry, float x, float y, float speed) : registry(registry) {
-    entity = registry.spawn_entity();
-    registry.add_component<Position>(entity, {x, y});
-    registry.add_component<Projectile>(entity, {speed});
-    registry.add_component<Drawable>(entity, {sf::RectangleShape(sf::Vector2f(5.0f, 5.0f))});
+    entity = this->registry.spawn_entity();
+    this->registry.add_component<Position>(entity, {x, y});
+    this->registry.add_component<Projectile>(entity, {speed});
+    this->registry.add_component<Drawable>(entity, {sf::RectangleShape(sf::Vector2f(5.0f, 5.0f))});
 }
 
 Bullet::~Bullet() {
